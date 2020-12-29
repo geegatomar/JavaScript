1745d7d830da372ed0a3f2ff61c330bc41825d5d
geegatomar <geegatomar@gmail.com>
2020-12-29 14:18:30 +0530

    js

---
+++ DevEd/app.js
@@ -0,0 +1,21 @@
+const name = "shiv";
+const age = 20;
+
+// String concatenation
+console.log(`my name is ${name} and age is ${age}`);
+
+
+// Creating an object
+const user = {
+    user_name: "Shiv j",
+    age: 30,
+    married: false,
+    purchase: ['phone', 'car', 'laptop']
+};
+
+function eat()
+{
+    console.log('apple');
+}
+
+console.log(this);
---
+++ HeadFirst_JavaScript_Book/app.js
@@ -0,0 +1,4 @@
+document.addEventListener('load', alert("Yes you got it"));
+const text = document.querySelector('.title');
+
+text.style.color = 'red';
