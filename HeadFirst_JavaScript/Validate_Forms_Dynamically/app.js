a44c7795a60918a413e006c3ad54c87a7d85be42
geegatomar <geegatomar@gmail.com>
2021-01-01 05:08:21 +0530

    js notes

--- HeadFirst_JavaScript/Validate_Forms_Dynamically/app.js
+++ HeadFirst_JavaScript/Validate_Forms_Dynamically/app.js
@@ -10,13 +10,26 @@ function validateName(thisForm)
 {
     console.log(typeof thisForm);                  // object
     console.log(typeof thisForm.value);            // string
-    console.log(typeof thisForm["name"]);
+    console.log(typeof thisForm["name"]);          // string
+    
     if(thisForm.value.length < 1 || thisForm.value.length > 32)
     {
         document.getElementById("name_help").textContent = "Please enter valid name";
+        // can also do .innerHTML
     }
     else
     {
         document.getElementById("name_help").textContent = "";
     }
 }
+
+/*
+class myClass{
+    name = "un";
+}
+
+var exampleobject = new myClass();
+
+console.log(typeof myClass);
+console.log(typeof exampleobject);
+*/
