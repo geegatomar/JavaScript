e48339ad19ab368c6b97c8825b5ef4dd684127cb
geegatomar <geegatomar@gmail.com>
2021-01-01 16:09:59 +0530

    adding tic tac toe

---
+++ DevEd/DOM_Manipulation/app.js
@@ -0,0 +1,12 @@
+const userList = document.querySelector(".name-list li");
+const listInput = document.querySelector(".list-input");
+const addListBtn = document.querySelector(".addListBtn");
+
+
+addListBtn.addEventListener("click", function(){
+    const newLi = document.createElement("LI");
+    const liContent = document.createTextNode(listInput.value);
+
+    newLi.appendChild(liContent);
+    userList.appendChild(newLi);
+});
